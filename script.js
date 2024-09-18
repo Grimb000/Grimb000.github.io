87cb3543ac1b3c16d4d96b52a5592495a8da9c17
Ilya Kislou <valorantik0005@gmail.com>
2024-09-18 10:43:27 +0300

    changed function hsv to rgb

--- script.js
+++ script.js
@@ -48,9 +48,42 @@ function rgbToCmyk(r, g, b) {
 }
 
 function hsvToRgb(h, s, v) {
+    if (s === 0) {
+        return [0, 0, 0];
+    }
+    h = h % 360;
+    const c = v * s; // Чистота
+    const x = c * (1 - Math.abs((h / 60) % 2 - 1));
+    const m = v - c;
     let r, g, b;
-    const f = (n, k = (n + h / 60) % 6) => v * (1 - s * Math.max(Math.min(k, 4 - k, 1), 0));
-    [r, g, b] = [0, 1, 2].map(n => Math.round(f(n) * 255));
+    if (h < 60) {
+        r = c;
+        g = x;
+        b = 0;
+    } else if (h < 120) {
+        r = x;
+        g = c;
+        b = 0;
+    } else if (h < 180) {
+        r = 0;
+        g = c;
+        b = x;
+    } else if (h < 240) {
+        r = 0;
+        g = x;
+        b = c;
+    } else if (h < 300) {
+        r = x;
+        g = 0;
+        b = c;
+    } else {
+        r = c;
+        g = 0;
+        b = x;
+    }
+    r = Math.round((r + m) * 255);
+    g = Math.round((g + m) * 255);
+    b = Math.round((b + m) * 255);
     return [r, g, b];
 }
 
@@ -60,7 +93,6 @@ function validateInput(value, min, max) {
 }
 
 const colorPicker = document.getElementById('colorPicker');
-
 
 colorPicker.addEventListener('input', (event) => {
     const hex = event.target.value;
